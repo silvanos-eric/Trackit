8a392261e9fad65e9c8ee4463a437280e1b8f490
Silvanos Eric <ericoderito@gmail.com>
2024-08-16 07:07:04 +0300

    feat(ui): enhance landing page with header, CTA button, and background image
    - Simplified layout in `App.jsx` by moving the navbar outside of the
      header
    - Added a hero section to `Landing.jsx` with a title, subtitle, and "Get
      Started" button
    - Integrated a new background image for the landing page
    - Updated CSS for improved layout and styling consistency

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,32 @@ import { Button, Container, Nav, Navbar } from "./components";
 
 const App = () => (
   <div className="d-flex flex-column h-100">
-    <header>
-      <Navbar expand="lg" className="">
-        <Container>
-          <Navbar.Brand className="fw-bold fs-1">
-            <Link to="/" className="text-decoration-none text-black">
-              Trackit.
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse
-            id="basic-navbar-nav"
-            className="justify-content-end"
-          >
-            <Nav className="d-flex gap-2">
-              <NavLink to="login">
-                <Button variant="outline-secondary">Sign In</Button>
-              </NavLink>
-              <NavLink to="register">
-                <Button variant="dark">Register</Button>
-              </NavLink>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
-    <main
+    <Navbar expand="lg" className="">
+      <Container>
+        <Navbar.Brand className="fw-bold fs-1">
+          <Link to="/" className="text-decoration-none text-black">
+            Trackit.
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="d-flex gap-2">
+            <NavLink to="login">
+              <Button variant="outline-secondary">Sign In</Button>
+            </NavLink>
+            <NavLink to="register">
+              <Button variant="dark">Register</Button>
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <div
       className="bg-secondary flex-grow-1"
       style={{ "--bs-bg-opacity": 0.1 }}
     >
       <Outlet />
-    </main>
+    </div>
   </div>
 );
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,24 @@
+import { Link } from "react-router-dom";
+import landingPageBgImage from "../assets/landing-page-bg-image.png";
 
-const Landing = () => <>Landing</>
+const Landing = () => (
+  <>
+    <header className="text-center">
+      <h1 className="fs-1 fw-bold mt-4">Trackit.</h1>
+      <h2>All your expenses in one place</h2>
+    </header>
+    <main className="text-center">
+      <Link to="/register">
+        <button
+          className="mt-4 text-white btn"
+          style={{ backgroundColor: "#FE6D00" }}
+        >
+          Get Started
+        </button>
+      </Link>
+      <img src={landingPageBgImage} alt="" className="d-block mx-auto mt-4" />
+    </main>
+  </>
+);
 
-export { Landing }
+export { Landing };
