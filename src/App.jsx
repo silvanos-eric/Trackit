dc51b2d8f13f0383f7bc45dd3c775d521dd66d14
Silvanos Eric <ericoderito@gmail.com>
2024-08-16 08:10:57 +0300

    feat(app): add 'Logout' button to the navbar
    - Introduced a 'Logout' button to the navbar with a basic click handler.
    - Refactored the `App` component from a functional expression to a
      functional declaration to accommodate the new logout functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,47 @@ import { Link, NavLink, Outlet } from "react-router-dom";
 import "./App.css";
 import { Button, Container, Nav, Navbar } from "./components";
 
-const App = () => (
-  <div className="d-flex flex-column h-100">
-    <Navbar expand="lg" className="">
-      <Container>
-        <Navbar.Brand className="fw-bold fs-1">
-          <Link to="/" className="text-decoration-none text-black">
-            Trackit.
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav className="d-flex gap-2">
-            <NavLink to="login">
-              <Button variant="outline-secondary">Sign In</Button>
-            </NavLink>
-            <NavLink to="register">
-              <Button variant="dark">Register</Button>
-            </NavLink>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <div
-      className="bg-secondary flex-grow-1"
-      style={{ "--bs-bg-opacity": 0.1 }}
-    >
-      <Outlet />
+const App = () => {
+  const logout = () => {
+    console.log("logout");
+  };
+
+  return (
+    <div className="d-flex flex-column h-100">
+      <Navbar expand="lg" className="">
+        <Container>
+          <Navbar.Brand className="fw-bold fs-1">
+            <Link to="/" className="text-decoration-none text-black">
+              Trackit.
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav className="d-flex gap-2">
+              <NavLink to="login">
+                <Button variant="outline-secondary">Sign In</Button>
+              </NavLink>
+              <NavLink to="register">
+                <Button variant="dark">Register</Button>
+              </NavLink>
+              <Button variant="secondary" onClick={logout}>
+                Logout
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div
+        className="bg-secondary flex-grow-1"
+        style={{ "--bs-bg-opacity": 0.1 }}
+      >
+        <Outlet />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export { App };
