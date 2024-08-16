4d27deafa2c5e032c5d4f247a2c9ebc2169309c9
David Wekesa <wekesadavid300@gmail.com>
2024-08-16 08:34:48 +0300

    david-components

---
+++ src/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <>
+      
+      <div>
+        <button style>Login</button>
+        <button style>Register</button>
+      </div>
+      <h1>Trackit</h1>
+      <p>All your experiences in one place</p>
+    </>
+  );
+}
+
+export default LandingPage;
