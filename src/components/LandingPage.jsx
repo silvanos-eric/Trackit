8d2f1360dafda3f6b6891d9575ed3f1eeeca722d
David Wekesa <wekesadavid300@gmail.com>
2024-08-16 08:37:28 +0300

    david-components

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div style={{ textAlign: 'center'}}>
+      
+      <div style={{ position: 'absolute', top: '20px', right: '20px' }}>
+        <button style>Login</button>
+        <button style>Register</button>
+      </div>
+
+      <h1 style>Trackit</h1>
+
+      <p style>All your experiences in one place</p>
+
+      <p style>Get Started</p>
+      
+    </div>
+  );
+}
+
+export default LandingPage;
